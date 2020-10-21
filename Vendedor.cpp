4c7cd777508764b729b4c1e9939ea4094535ca78
lucasdrte <73208615+lucasdrte@users.noreply.github.com>
2020-10-20 23:51:31 -0300

    2014015877
    Implementação de "quotaTotalAnual" movida para um novo arquivo "Vendedor.cpp"

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -6,9 +6,7 @@ class Vendedor : public Empregado {
   public:
     std::string nome;  	  
 	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
+	double quotaTotalAnual() {}
 	
 };
 
